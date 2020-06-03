7715c3c4dd472caa7a0fd2885daa62c27ff0cfc7
Lior Rahamim <kenlelior@gmail.com>
2020-06-03 20:11:29 +0300

    Can add movies to firebase without photos

--- app/src/main/java/com/example/moovy/EditActivity.java
+++ app/src/main/java/com/example/moovy/EditActivity.java
@@ -15,18 +15,54 @@ package com.example.moovy;
     import android.view.Menu;
     import android.view.MenuItem;
     import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
     import android.widget.ImageView;
+    import android.widget.Toast;
 
+    import com.google.android.gms.tasks.OnSuccessListener;
+    import com.google.firebase.database.DatabaseReference;
+    import com.google.firebase.database.FirebaseDatabase;
+    import com.google.firebase.firestore.DocumentReference;
+    import com.google.firebase.firestore.FirebaseFirestore;
+
+    import java.io.Console;
+    import java.io.DataInput;
     import java.io.File;
+    import java.util.Date;
+
+    import models.Movie;
 
 public class EditActivity extends AppCompatActivity {
     private ImageView imageView;
 
+    private EditText nameInput, genreInput, directorInput, starringInput, summaryInput;
+    private Button updateButton, deleteButton;
+    private Movie movie;
+    //private DatabaseReference reff;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_edit);
         imageView = (ImageView) findViewById(R.id.imageView2);
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        genreInput = (EditText) findViewById(R.id.genreInput);
+        directorInput = (EditText) findViewById(R.id.directorInput);
+        starringInput = (EditText) findViewById(R.id.starringInput);
+        summaryInput = (EditText) findViewById(R.id.summaryInput);
+        updateButton = (Button) findViewById(R.id.updateButton);
+        deleteButton = (Button) findViewById(R.id.deleteButton);
+        movie = new Movie(
+                nameInput.getText().toString().trim(),
+                genreInput.getText().toString().trim(),
+                directorInput.getText().toString().trim(),
+                starringInput.getText().toString().trim(),
+                summaryInput.getText().toString());
+        movie = new Movie();
+        //reff = FirebaseDatabase.getInstance().getReference();
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +70,31 @@ public class EditActivity extends AppCompatActivity {
                 selectImage(EditActivity.this);
             }
         });
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                movie.setName(nameInput.getText().toString().trim());
+                movie.setGenre(genreInput.getText().toString().trim());
+                movie.setDirector(directorInput.getText().toString().trim());
+                movie.setStarring(starringInput.getText().toString().trim());
+                movie.setSummary(summaryInput.getText().toString());
+
+                db.collection("movies").add(movie)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(
+                                        EditActivity.this,
+                                        "Movie added",
+                                        Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        });
+
+            }
+        });
+
     }
 
     private void selectImage(Context context) {
