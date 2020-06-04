9b85862dfa07f96f3613bc027f47c1f432ca76ef
OfekAziel <209492875@idf.il>
2020-06-04 20:36:16 +0300

    fix function protected

--- app/src/main/java/com/example/moovy/ui/LoginActivity.java
+++ app/src/main/java/com/example/moovy/ui/LoginActivity.java
@@ -27,7 +27,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
 
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         firebaseAuth = FirebaseAuth.getInstance();
