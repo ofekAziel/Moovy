95812dee997a1efba84441f90157027467cb4d2c
Ofek <209492875@idf.il>
2020-07-02 15:27:54 +0300

    refactoring + making the back button work

--- app/src/main/java/com/example/moovy/ui/DetailsActivity.java
+++ app/src/main/java/com/example/moovy/ui/DetailsActivity.java
@@ -10,18 +10,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.moovy.R;
 
 public class DetailsActivity extends AppCompatActivity {
-    private ImageButton editButton;
+    private ImageButton editButton, returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        returnButton = findViewById(R.id.returnButton);
+        editButton = findViewById(R.id.editButton);
+        returnButtonClickListener();
+        editButtonClickListener();
+    }
 
-        editButton = (ImageButton) findViewById(R.id.editButton);
+    private void returnButtonClickListener() {
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void editButtonClickListener() {
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // open edit activity with certain movie
                 Intent intent = new Intent(DetailsActivity.this, EditActivity.class);
                 startActivity(intent);
             }
