b86a95c0781a685e9fb9c3f29944e2107adebbc6
OfekAziel <209492875@idf.il>
2020-07-13 23:14:32 +0300

    imports

--- app/src/main/java/com/example/moovy/ui/MainActivity.java
+++ app/src/main/java/com/example/moovy/ui/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -29,7 +28,6 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.gson.Gson;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
