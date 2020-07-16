61165b53cc183597cfcfcd3c4a8b51c2fbb2c257
OfekAziel <209492875@idf.il>
2020-07-16 21:55:29 +0300

    added the saving new user when signed up

--- app/src/main/java/com/example/moovy/ui/SignUpActivity.java
+++ app/src/main/java/com/example/moovy/ui/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.example.moovy.ui;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,27 +9,36 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.moovy.R;
+import com.example.moovy.models.User;
 import com.example.moovy.services.ValidationService;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private EditText username, password;
+    private EditText firstName, lastName, username, password;
     private Button signUpButton;
     private FirebaseAuth firebaseAuth;
     private ValidationService validationService = new ValidationService();
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         firebaseAuth = FirebaseAuth.getInstance();
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         signUpButton = findViewById(R.id.signUp);
@@ -39,32 +49,55 @@ public class SignUpActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                User user = new User(firstName.getText().toString(), lastName.getText().toString());
                 String usernameText = username.getText().toString();
                 String passwordText = password.getText().toString();
 
-                if (!validationService.isUserNameValid(usernameText)) {
-                    username.setError("Not valid username");
+                if(user.getFirstName().isEmpty()) {
+                    firstName.setError("Please enter a first name");
+                    firstName.requestFocus();
+                }
+                else if(user.getLastName().isEmpty()) {
+                    lastName.setError("Please enter a last name");
+                    lastName.requestFocus();
+                }
+                else if (!validationService.isUserNameValid(usernameText)) {
+                    username.setError("Please enter a valid Email address");
                     username.requestFocus();
                 } else if (!validationService.isPasswordValid(passwordText)) {
-                    password.setError("Not valid password");
+                    password.setError("Password must contain at least 5 characters");
                     password.requestFocus();
                 } else {
-                    signUpFirebase(usernameText, passwordText);
+                    signUpFirebase(usernameText, passwordText, user);
                 }
             }
         });
     }
 
-    private void signUpFirebase(String usernameText, String passwordText) {
+    private void signUpFirebase(String usernameText, String passwordText, final User user) {
         firebaseAuth.createUserWithEmailAndPassword(usernameText, passwordText).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (!task.isSuccessful()) {
                     Toast.makeText(SignUpActivity.this, "Cannot signUp", Toast.LENGTH_SHORT).show();
                 } else {
+                    String userUid = Objects.requireNonNull(Objects.requireNonNull(task.getResult()).getUser()).getUid();
+                    saveUserToDatabase(user, userUid);
                     Intent mainIntent = new Intent(SignUpActivity.this, MainActivity.class);
                     SignUpActivity.this.startActivity(mainIntent);
                 }
+            }
+        });
+    }
+
+    private void saveUserToDatabase(User user, String userUid) {
+        user.setUserUid(userUid);
+        user.setAdmin(false);
+        db.collection("users").add(user).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this, "Cannot save user", Toast.LENGTH_SHORT).show();
             }
         });
     }
