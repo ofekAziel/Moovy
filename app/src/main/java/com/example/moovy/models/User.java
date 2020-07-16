76df8119de2873b7fca3ad4dcd98f3aaa460a5c2
OfekAziel <209492875@idf.il>
2020-07-16 23:36:51 +0300

    added serialization to user entity

--- app/src/main/java/com/example/moovy/models/User.java
+++ app/src/main/java/com/example/moovy/models/User.java
@@ -1,6 +1,8 @@
 package com.example.moovy.models;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 
     private String firstName, lastName, userUid;
     private boolean isAdmin;
