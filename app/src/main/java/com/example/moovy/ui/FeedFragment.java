6e29394d34ac3d9448a8902aaa21bf8b2e158a90
Lior Rahamim <kenlelior@gmail.com>
2020-08-06 07:42:28 +0300

    Added FeedFragment, LogInFragment and SignUpFragment

---
+++ app/src/main/java/com/example/moovy/ui/FeedFragment.java
@@ -0,0 +1,186 @@
+package com.example.moovy.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.moovy.R;
+import com.example.moovy.models.Movie;
+import com.example.moovy.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class FeedFragment extends Fragment {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    GridView movieGrid;
+    TextView currentUser;
+    User user;
+    Button addMovieButton;
+    List<Movie> movies = new ArrayList<>();
+
+    public FeedFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        movieGrid = getView().findViewById(R.id.movieGrid);
+        addMovieButton = getView().findViewById(R.id.addMovieButton);
+        currentUser = getView().findViewById(R.id.currentUser);
+        getCurrentUser();
+        addMovieButtonClickListener();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        movies.clear();
+        selectAllMovies();
+    }
+
+    private void setUpScreenAdmin() {
+        String userDisplayName = "Hello " + user.getFirstName() + " " + user.getLastName();
+        currentUser.setText(userDisplayName);
+
+        if (!user.isAdmin()) {
+            addMovieButton.setVisibility(View.GONE);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void getCurrentUser() {
+        String currentUserUid = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid();
+        db.collection("users").whereEqualTo("userUid", currentUserUid).limit(1).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                user = queryDocumentSnapshots.getDocuments().get(0).toObject(User.class);
+                setUpScreenAdmin();
+            }
+        });
+    }
+
+    private void addMovieButtonClickListener() {
+        addMovieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent mainIntent = new Intent(MainActivity.this, EditActivity.class);
+                //MainActivity.this.startActivity(mainIntent);
+            }
+        });
+    }
+
+    private void selectAllMovies() {
+        db.collection("movies").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                        Movie movie = document.toObject(Movie.class);
+                        movie.setId(document.getId());
+                        movies.add(movie);
+                    }
+
+                    FeedFragment.CustomAdapter customAdapter = new FeedFragment.CustomAdapter();
+                    movieGrid.setAdapter(customAdapter);
+                }
+            }
+        });
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return movies.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return movies.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.movie_layout, null);
+            movieCardClickListener(view, position);
+            downloadMoviePhoto(view, movies.get(position).getPhotoHash());
+            setMovieCardFields(position, view);
+            return view;
+        }
+
+        private void setMovieCardFields(int position, View view) {
+            TextView movieName = view.findViewById(R.id.movieName);
+            TextView movieGenre = view.findViewById(R.id.movieGenre);
+            TextView movieRating = view.findViewById(R.id.movieRating);
+
+            movieName.setText(movies.get(position).getName());
+            movieGenre.setText(movies.get(position).getGenre());
+            movieRating.setText(String.valueOf(movies.get(position).getAverageRating()).substring(0, 3));
+        }
+
+        private void downloadMoviePhoto(View view, final int photoHash) {
+            StorageReference imageReference = FirebaseStorage.getInstance().getReference().child("moviePhotos/" + photoHash);
+            ImageView moviePhoto = view.findViewById(R.id.moviePhoto);
+            GlideApp.with(view.getContext()).load(imageReference).into(moviePhoto);
+        }
+
+        private void movieCardClickListener(View view, int position) {
+            final Movie selectedMovie = (Movie) this.getItem(position);
+            ConstraintLayout movieCard = view.findViewById(R.id.movieCard);
+            movieCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent mainIntent = new Intent(context, DetailsActivity.class);
+                    mainIntent.putExtra("selectedMovie", selectedMovie);
+                    mainIntent.putExtra("user", user);
+                    context.startActivity(mainIntent);
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/moovy/ui/SignUpFragment.java
@@ -0,0 +1,116 @@
+package com.example.moovy.ui;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.moovy.R;
+import com.example.moovy.models.User;
+import com.example.moovy.services.ValidationService;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+public class SignUpFragment extends Fragment {
+
+    private EditText firstName, lastName, username, password;
+    private Button signUpButton;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private ValidationService validationService = new ValidationService();
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public SignUpFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //setContentView(R.layout.activity_sign_up);
+        firstName = getView().findViewById(R.id.firstName);
+        lastName = getView().findViewById(R.id.lastName);
+        username = getView().findViewById(R.id.username);
+        password = getView().findViewById(R.id.password);
+        signUpButton = getView().findViewById(R.id.signUp);
+        signUpButtonClickListener();
+    }
+
+    private void signUpButtonClickListener() {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User user = new User(firstName.getText().toString(), lastName.getText().toString());
+                String usernameText = username.getText().toString();
+                String passwordText = password.getText().toString();
+
+                if(user.getFirstName().isEmpty()) {
+                    firstName.setError("Please enter a first name");
+                    firstName.requestFocus();
+                }
+                else if(user.getLastName().isEmpty()) {
+                    lastName.setError("Please enter a last name");
+                    lastName.requestFocus();
+                }
+                else if (!validationService.isUserNameValid(usernameText)) {
+                    username.setError("Please enter a valid Email address");
+                    username.requestFocus();
+                } else if (!validationService.isPasswordValid(passwordText)) {
+                    password.setError("Password must contain at least 5 characters");
+                    password.requestFocus();
+                } else {
+                    signUpFirebase(usernameText, passwordText, user);
+                }
+            }
+        });
+    }
+
+    private void signUpFirebase(String usernameText, String passwordText, final User user) {
+        firebaseAuth.createUserWithEmailAndPassword(usernameText, passwordText).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(getContext(), "Cannot signUp", Toast.LENGTH_SHORT).show();
+                } else {
+                    String userUid = Objects.requireNonNull(Objects.requireNonNull(task.getResult()).getUser()).getUid();
+                    saveUserToDatabase(user, userUid);
+                    Navigation.findNavController(getView()).navigate(R.id.action_signUpFragment_to_feedFragment);
+                }
+            }
+        });
+    }
+
+    private void saveUserToDatabase(User user, String userUid) {
+        user.setUserUid(userUid);
+        user.setAdmin(false);
+        db.collection("users").add(user).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Cannot save user", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
