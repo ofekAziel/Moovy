520a0e8fbfa7d660ba0cf555bed5bee9b734fec8
OfekAziel <209492875@idf.il>
2020-08-26 22:11:07 +0300

    fix user loading

--- app/src/main/java/com/example/moovy/repositories/UserRepository.java
+++ app/src/main/java/com/example/moovy/repositories/UserRepository.java
@@ -43,6 +43,7 @@ public class UserRepository {
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 if (!queryDocumentSnapshots.getDocuments().isEmpty()) {
                     user = queryDocumentSnapshots.getDocuments().get(0).toObject(User.class);
+                    new AddUserAsyncTask(userDao).execute(user);
                 }
             }
         });
